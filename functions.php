211b32ccb744ff6f6e2f0ee181aaf0d4f0211f6c
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-05-23 03:57:03 +0300

    cleared all comments

--- functions.php
+++ functions.php
@@ -8,55 +8,20 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
 function nepaleseinfinland_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on NepaleseinFinland, use a find and replace
-		* to change 'nepaleseinfinland' to the name of your theme in all the template files.
-		*/
+
 	load_theme_textdomain( 'nepaleseinfinland', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
 	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
 	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'nepaleseinfinland' ),
 		)
 	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
 	add_theme_support(
 		'html5',
 		array(
@@ -70,7 +35,6 @@ function nepaleseinfinland_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
 	add_theme_support(
 		'custom-background',
 		apply_filters(
@@ -81,15 +45,7 @@ function nepaleseinfinland_setup() {
 			)
 		)
 	);
-
-	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
 	add_theme_support(
 		'custom-logo',
 		array(
@@ -102,23 +58,10 @@ function nepaleseinfinland_setup() {
 }
 add_action( 'after_setup_theme', 'nepaleseinfinland_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
 function nepaleseinfinland_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'nepaleseinfinland_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'nepaleseinfinland_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function nepaleseinfinland_widgets_init() {
 	register_sidebar(
 		array(
@@ -134,9 +77,6 @@ function nepaleseinfinland_widgets_init() {
 }
 add_action( 'widgets_init', 'nepaleseinfinland_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
 function nepaleseinfinland_scripts() {
 	wp_enqueue_style( 'nepaleseinfinland-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'nepaleseinfinland-style', 'rtl', 'replace' );
@@ -149,29 +89,11 @@ function nepaleseinfinland_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'nepaleseinfinland_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
 require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
