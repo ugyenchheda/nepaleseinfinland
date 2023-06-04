365619d4d9fe699b972599583c7772e0106919cb
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-06-04 18:32:22 +0300

    added meta for news post type

--- inc/post-types-meta.php
+++ inc/post-types-meta.php
@@ -15,13 +15,50 @@ function news_post_meta()
 
     ));
 
-
     $cmb->add_field(array(
 		'name' => 'Hot News',
 		'desc' => 'Check if this is hot news.',
 		'id'   => 'news_hot',
 		'type' => 'checkbox',
     ));
+
+    $cmb->add_field(array(
+		'name' => 'Author',
+		'desc' => 'Add the name of the author.',
+		'id'   => 'news_author',
+		'type' => 'text',
+    ));
+
+    $cmb->add_field(array(
+		'name' => 'Author',
+		'desc' => 'Add the name of the author.',
+		'id'   => 'news_author',
+		'type' => 'text',
+    ))
+    ;$cmb->add_field( array(
+      'name'    => 'News Image',
+      'desc'    => 'Upload an image or enter an URL.',
+      'id'      => 'news_image',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+          'url' => false, // Hide the text input for the url
+      ),
+      'text'    => array(
+          'add_upload_file_text' => 'Add File' // Change upload button text. Default: "Add or Upload File"
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+          'type' => 'application/pdf', // Make library only display PDFs.
+          // Or only allow gif, jpg, or png images
+          // 'type' => array(
+          //     'image/gif',
+          //     'image/jpeg',
+          //     'image/png',
+          // ),
+      ),
+      'preview_size' => 'large', // Image size to use when previewing in the admin.
+  ) );
 
 }
 
