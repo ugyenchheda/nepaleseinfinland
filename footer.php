6fbf0fef7438e33b38c72bf2576dd243fa359560
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-06-11 22:41:06 +0300

    footer arrangement

--- footer.php
+++ footer.php
@@ -269,17 +269,6 @@
             </div>
         </div>
     </div>
-	<footer id="colophon" class="sitemain-footer">
-		<div class="container">
-			<div class="text-center copyright-text"> © 2022-<?php echo date("Y");?> 
-			<span class="sep"> | </span>
-				<span class="copyright-text">
-					<?php
-						printf( esc_html__( 'Developed and Powered by: %2$s.', 'nepaleseinfinland' ), 'nepaleseinfinland', '<a href="https://ugyen.com.np/">Ugyen</a>' );
-					?>
-				</span>
-		</div></div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- page.php
+++ page.php
@@ -2103,23 +2103,7 @@ get_header();
     <!--====== FOOTER PART START ======-->
 
 
-    <div class="footer-copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="copyright-item d-block d-md-flex justify-content-between align-items-center">
-                        <p>© Copyright 2020, All Rights Reserved</p>
-                        <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Advertise</a></li>
-                            <li><a href="#">Privacy & Policy</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
     <!--====== FOOTER PART ENDS ======-->
 
