9df8abd330e960cb76e7f9d06a1ba02353c19776
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-06-14 22:20:12 +0300

    comment text area moved to bottom

--- functions.php
+++ functions.php
@@ -225,3 +225,12 @@ function good_comment_policy($arg) {
 	}
 	 
 	add_filter('comment_form_defaults', 'good_comment_policy');
+
+	function move_comment_form_to_bottom( $fields ) {
+		$comment_field = $fields['comment'];
+		unset( $fields['comment'] );
+		$fields['comment'] = $comment_field;
+		return $fields;
+		}
+		 
+		add_filter( 'comment_form_fields', 'move_comment_form_to_bottom');
