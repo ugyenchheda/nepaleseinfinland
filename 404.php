7a49d79e3eae7cf41784f511051d54b7df33d401
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-07-05 18:21:38 +0300

    404 page set up

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header();
 <section class="error-area">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-lg-9">
+                <div class="col-lg-7">
                     <div class="error-thumb text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404.jpg" alt="">
                     </div>
