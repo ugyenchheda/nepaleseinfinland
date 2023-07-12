4db1d4deec2cf66c8777f216889a46b5cc57323e
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-07-13 01:09:12 +0300

    done with search result page

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="section-padding">
+<section class="section-padding search-result-pad">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -14,25 +14,31 @@
                     </nav>
                 </div>
             </div>
-            <div class="col-lg-8">
+            <div class="col-lg-12">
                 <div class="about-tab-btn mt-40">
-                    <div class="archive-btn">
-                        <ul>
-                            <li><?php printf( esc_html__( 'You searched for: %s', 'nepaleseinfinland' ), '<span>' . get_search_query() . '</span>' ); ?></li>
-                        </ul>
+                    <div class="archive-btn for-search">
+						<?php printf( esc_html__( 'You searched for: %s', 'nepaleseinfinland' ), '<span class="searchresult-topic">' . get_search_query() . '</span>' ); ?>
                     </div>
                     <div class="about-post-items">
                         <div class="row">
-                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                                <div class="col-lg-6 col-md-6">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                                <div class="col-lg-4 col-md-4">
                                     <div class="trending-image-post feature-item mt-30">
-                                        <?php echo get_the_post_thumbnail(get_the_ID(), 'post_image_l'); ?>
+                                        <?php //echo get_the_post_thumbnail(get_the_ID(), 'post_image_l'); 
+										$thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'post_image_l');
+
+										// Display the thumbnail image
+										echo '<img src="' . $thumbnail_url . '" width="387" height="242">';?>
+										
                                         <div class="trending-image-content" id="post-<?php the_ID(); ?>">
                                             <div class="post-meta">
                                                 <div class="meta-categories my_posttypes">
                                                     <?php 
-                                                        $post_type = get_post_type(); 
-                                                        echo $post_type;
+														$post_type = get_post_type(); 
+														$post_type_object = get_post_type_object($post_type);
+														if ($post_type_object) {
+															echo $post_type_object->labels->name;
+														}
                                                     ?>
                                                 </div>
                                                 <?php
@@ -59,35 +65,35 @@
                                         </div>
                                     </div>
                                 </div>
-                            <?php endwhile; endif; ?>
+                            
+								<?php endwhile; endif; ?>
                         </div>
-                        <?php
-                        // Pagination
-                        global $wp_query;
-                        $big = 999999999; // need an unlikely integer
+							<?php
+								global $wp_query;
+								$big = 999999999; // need an unlikely integer
 
-                        $pagination = paginate_links(array(
-                            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-                            'format' => '?paged=%#%',
-                            'current' => max(1, get_query_var('paged')),
-                            'total' => $wp_query->max_num_pages,
-                            'prev_text' => '<span aria-hidden="true"><i class="fas fa-caret-left"></i></span>',
-                            'next_text' => '<span aria-hidden="true"><i class="fas fa-caret-right"></i></span>',
-                            'type'  => 'array',
-                        ));
-
-                        if ($pagination) :
-                            ?>
-                            <div class="pagination-item pt-40">
-                                <nav aria-label="Page navigation example">
-                                    <ul class="pagination">
-                                        <?php foreach ($pagination as $page) : ?>
-                                            <li><?php echo $page; ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                </nav>
-                            </div>
-                        <?php endif; ?>
+								$pages = paginate_links( array(
+										'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+										'format' => '?paged=%#%',
+										'current' => max( 1, get_query_var('paged') ),
+										'total' => $wp_query->max_num_pages,
+										'prev_text' => '<span aria-hidden="true"><i class="fas fa-caret-left"></i></span>',
+										'next_text' => '<span aria-hidden="true"><i class="fas fa-caret-right"></i></span>',
+										'type'  => 'array',
+									) );
+							if( is_array( $pages ) ) {
+								$paged = ( get_query_var('paged') == 0 ) ? 1 : get_query_var('paged'); ?>
+								<div class="pagination-item pt-40">
+									<nav aria-label="Page navigation example">
+										<ul class="pagination">
+											<?php
+											foreach ( $pages as $page ) { ?>
+												<li><?php echo $page; ?></li>
+											<?php } ?>
+										</ul>
+									</nav>
+								</div>
+							<?php } ?>      
                     </div>
                 </div>
             </div>
