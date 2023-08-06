ba85edb9869c57626c1b441b8fad0006f7c366db
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-06 20:33:34 +0300

    clearing comments

--- index.php
+++ index.php
@@ -27,16 +27,8 @@ get_header();
 				</header>
 				<?php
 			endif;
-
-			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
